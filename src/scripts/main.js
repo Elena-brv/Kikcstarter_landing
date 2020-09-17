abf844dc5eb703471aefcced9ad5b2bb4d529391
Elena-brv <leena.brv@gmail.com>
2020-09-17 21:45:35 +0300

    video was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const recordPlayer = document.querySelector('.presentation__record-player');
+const video = document.querySelector('.presentation__video');
+
+recordPlayer.addEventListener('click', () => {
+  recordPlayer.style.display = 'none';
+  video.style.display = 'block';
+
+  video.setAttribute(
+    'src',
+    'https://www.youtube.com/embed/T3psVSj1YC8?&autoplay=1'
+  );
+});
